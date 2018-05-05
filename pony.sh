ac85853a4b00a7b555eed24fffec42235859ecf7
harens <harensdeveloper@gmail.com>
2018-05-05 15:27:07 +0100

    Update pony.sh

--- pony.sh
+++ pony.sh
@@ -3,5 +3,5 @@
 # The user runs this file, and the following commands opens up the Shell Edition
 # This run file shortens what you need to type to start the program
 cd
-cd ThePonyInformationProject
+cd PonySh
 bash index.sh
